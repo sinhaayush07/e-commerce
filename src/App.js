6fb410c965703b508d771631f8c35524a0d18e07
Ayush Sinha <toayushofficial@gmail.com>
2020-04-26 19:37:56 +0530

    added basic styling and homepage markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Homepage from './homepage.component'
+
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <Homepage />
+    )
   }
 }
 
